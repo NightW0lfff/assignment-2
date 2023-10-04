c16eaf92e0344059e958697afb894f4e88f243ca
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2023-10-04 19:19:11 +1100

    Add elements and css properties

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,40 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="maincard">
+    <div className="container">
       <h1>Contactor</h1>
-      <input type="text" />
-      <button className="add-contact-btn"></button>
-      <div className="contact-card">
-        <div></div>
+      <div className="main-card">
+        <h2>Contacts</h2>
+        <input className="contact-name input" type="text" placeholder="Name" />
+        <button className="contact add-btn">Create Contact</button>
+        <div className="contact-card">
+          <div className="contact input-container">
+            <h3 className="contact-name">Name</h3>
+            <button className="contact del-btn">Delete</button>
+          </div>
+          <div className="phone-card">
+            <div className="phone input-container">
+              <input
+                className="phone-type input"
+                type="text"
+                placeholder="Type"
+              />
+              <input
+                className="phone-number input"
+                type="text"
+                placeholder="Phone Number"
+              />
+              <button className="phone add-btn">Add</button>
+              <div className="contact-table">
+                <h4>Type</h4>
+                <h4>Contact Number</h4>
+                <button className="phone delete-btn">Delete</button>
+                {/* Type input from database using API */}
+                {/* Contact number get from database using API */}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
